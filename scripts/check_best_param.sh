5c536bba9c21dee65724759b7e171a72e271bf61
BBQ_God <1827691276a@sina.com>
2024-12-13 13:33:45 +0800

    fix scripts

--- scripts/check_best_param.sh
+++ scripts/check_best_param.sh
@@ -3,5 +3,5 @@
 cd rrl-DM_HW/scripts
 
 python check_best_param.py ../log_folder/bank-marketing > log.bank-marketing.txt True
-python check_best_param.py ../log_folder/breast-cancer > log.bank-marketing.txt False
-# python check_best_param.py ../log_folder/bank-marketing > log.bank-marketing.txt True
+# python check_best_param.py ../log_folder/breast-cancer > log.breast-cancer.txt False
+# python check_best_param.py ../log_folder/boston-housing > log.boston-housing.txt True
--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -5,7 +5,7 @@ cd rrl-DM_HW
 
 available_gpus=(2 1 0)
 
-# dataset=bank-marketing
+dataset=bank-marketing
 
 # 网格搜索超参数列表
 # learning_rates=(0.0002 0.001 0.005)          # 学习率
@@ -14,118 +14,118 @@ available_gpus=(2 1 0)
 # weight_decays=(0.0001 0.01 0.00001 0)          # 权重衰减
 
 # 网格搜索结构参数列表
-# learning_rates=(0.001)
-# temperatures=(1.0)
-# structures=("4@64" "16@64" "64@64" "128@64" "128@1" "128@16" "128@1024")
-# weight_decays=(0)
+learning_rates=(0.001)
+temperatures=(1.0)
+structures=("4@64" "16@64" "64@64" "128@64" "128@1" "128@16" "128@1024")
+weight_decays=(0)
 
 gpu_count=${#available_gpus[@]}  # 可用GPU数量
-# current_job=0  # 当前实验计数器
+current_job=0  # 当前实验计数器
 
-# # 迭代网格搜索
-# for lr in "${learning_rates[@]}"
-# do
-#     for temp in "${temperatures[@]}"
-#     do
-#         for structure in "${structures[@]}"
-#         do
-#             for wd in "${weight_decays[@]}"
-#             do
-#                 for i in 0 1 2 3 4
-#                 do
-#                     # 获取当前可用的 GPU
-#                     gpu_id=${available_gpus[$current_job%gpu_count]}
+# 迭代网格搜索
+for lr in "${learning_rates[@]}"
+do
+    for temp in "${temperatures[@]}"
+    do
+        for structure in "${structures[@]}"
+        do
+            for wd in "${weight_decays[@]}"
+            do
+                for i in 0 1 2 3 4
+                do
+                    # 获取当前可用的 GPU
+                    gpu_id=${available_gpus[$current_job%gpu_count]}
                     
-#                     # 启动实验
-#                     CUDA_VISIBLE_DEVICES=$gpu_id \
-#                     python experiment.py \
-#                         -d $dataset \
-#                         -t classification \
-#                         -bs 32 \
-#                         -s $structure \
-#                         -e401 \
-#                         -lrde 100 \
-#                         -lr $lr \
-#                         -ki $i \
-#                         -i 0 \
-#                         -wd $wd \
-#                         --temp $temp \
-#                         --save_best \
-#                         --print_rule &
+                    # 启动实验
+                    CUDA_VISIBLE_DEVICES=$gpu_id \
+                    python experiment.py \
+                        -d $dataset \
+                        -t classification \
+                        -bs 32 \
+                        -s $structure \
+                        -e401 \
+                        -lrde 100 \
+                        -lr $lr \
+                        -ki $i \
+                        -i 0 \
+                        -wd $wd \
+                        --temp $temp \
+                        --save_best \
+                        --print_rule &
 
-#                     # 增加实验计数器
-#                     current_job=$((current_job + 1))
+                    # 增加实验计数器
+                    current_job=$((current_job + 1))
 
-#                     # 如果达到 GPU 数量的上限，暂停并等待所有后台作业完成
-#                     if (( current_job % gpu_count == 0 )); then
-#                         wait  # 等待GPU资源释放
-#                     fi
-#                 done
-#             done
-#         done
-#     done
-# done
+                    # 如果达到 GPU 数量的上限，暂停并等待所有后台作业完成
+                    if (( current_job % gpu_count == 0 )); then
+                        wait  # 等待GPU资源释放
+                    fi
+                done
+            done
+        done
+    done
+done
 
-# # 等待所有剩余的后台工作完成
-# wait
+# 等待所有剩余的后台工作完成
+wait
 
-# echo "$dataset done"
+echo "$dataset done"
 
 
-# dataset="breast-cancer-{}"
+dataset="breast-cancer-{}"
 
-# # 网格搜索结构参数列表
-# learning_rates=(0.001)
-# temperatures=(1.0)
-# structures=("4@32" "16@32" "32@16" "32@8" "32@4" "32@1")
-# weight_decays=(0)
+# 网格搜索结构参数列表
+learning_rates=(0.001)
+temperatures=(1.0)
+structures=("4@32" "16@32" "32@16" "32@8" "32@4" "32@1")
+weight_decays=(0)
 
-# current_job=0  # 当前实验计数器
+current_job=0  # 当前实验计数器
 
-# # 迭代网格搜索
-# for lr in "${learning_rates[@]}"
-# do
-#     for temp in "${temperatures[@]}"
-#     do
-#         for structure in "${structures[@]}"
-#         do
-#             for wd in "${weight_decays[@]}"
-#             do
-#                 # 获取当前可用的 GPU
-#                 gpu_id=${available_gpus[$current_job%gpu_count]}
+# 迭代网格搜索
+for lr in "${learning_rates[@]}"
+do
+    for temp in "${temperatures[@]}"
+    do
+        for structure in "${structures[@]}"
+        do
+            for wd in "${weight_decays[@]}"
+            do
+                # 获取当前可用的 GPU
+                gpu_id=${available_gpus[$current_job%gpu_count]}
                 
-#                 # 启动实验
-#                 CUDA_VISIBLE_DEVICES=$gpu_id \
-#                 python experiment.py \
-#                     -d $dataset \
-#                     -t classification-test \
-#                     -bs 32 \
-#                     -s $structure \
-#                     -e401 \
-#                     -lrde 100 \
-#                     -lr $lr \
-#                     -i 0 \
-#                     -wd $wd \
-#                     --temp $temp \
-#                     --save_best \
-#                     --print_rule &
+                # 启动实验
+                CUDA_VISIBLE_DEVICES=$gpu_id \
+                python experiment.py \
+                    -d $dataset \
+                    -t classification-test \
+                    -bs 32 \
+                    -s $structure \
+                    -e401 \
+                    -lrde 100 \
+                    -lr $lr \
+                    -i 0 \
+                    -wd $wd \
+                    --temp $temp \
+                    --save_best \
+                    --print_rule &
 
-#                 # 增加实验计数器
-#                 current_job=$((current_job + 1))
+                # 增加实验计数器
+                current_job=$((current_job + 1))
 
-#                 # 如果达到 GPU 数量的上限，暂停并等待所有后台作业完成
-#                 if (( current_job % gpu_count == 0 )); then
-#                     wait  # 等待GPU资源释放
-#                 fi
-#             done
-#         done
-#     done
-# done
+                # 如果达到 GPU 数量的上限，暂停并等待所有后台作业完成
+                if (( current_job % gpu_count == 0 )); then
+                    wait  # 等待GPU资源释放
+                fi
+            done
+        done
+    done
+done
 
-# # 等待所有剩余的后台工作完成
-# wait
+# 等待所有剩余的后台工作完成
+wait
 
-# echo "$dataset done"
+echo "$dataset done"
 
 
 dataset="boston-housing"
@@ -133,8 +133,7 @@ dataset="boston-housing"
 # 网格搜索结构参数列表
 learning_rates=(0.001)
 temperatures=(1.0)
-structures=("16@64" "64@64" "128@64" "128@1" "128@16" "128@1024")
-# structures=("4@64")
+structures=("4@64" "16@64" "64@64" "128@64" "128@1" "128@16" "128@1024")
 weight_decays=(0)
 
 current_job=0  # 当前实验计数器
